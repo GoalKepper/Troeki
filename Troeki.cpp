af97982fc84ce631a3c86f41cfc2b590f4661686
GoalKepper <Al.gridnev2002@yandex.ru>
2020-11-18 19:58:49 +0300

    Добавьте файлы проекта.

---
+++ Troeki.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+int main()
+{
+	char A[1000];
+	int i = 0;
+	int k = 0;
+	do {
+		A[i] = getchar();
+		if (A[i] == '\n') break;
+		i++;
+		if ((A[i] == 'б') && (A[i - 1] == ' '))
+		{
+			k++;
+		}
+	} while (1);
+	printf("%c", k);
+	_getch();
+	return 0;
+}
